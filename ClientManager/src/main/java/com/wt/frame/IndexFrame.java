5a5773aa124437d4c5fcc851ddf3f6120043eb17
xtx <2513539241@qq.com>
2020-12-30 11:04:58 +0800

    banner & cardlayout

--- ClientManager/src/main/java/com/wt/frame/IndexFrame.java
+++ ClientManager/src/main/java/com/wt/frame/IndexFrame.java
@@ -7,6 +7,8 @@ import org.apache.poi.ss.formula.functions.Index;
 import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 /**
  * @ClassName IndexFrame
@@ -18,6 +20,23 @@ public class IndexFrame extends JFrame{
     private JPanel mainPanel;
     private JPanel menuPanel;
     private JLabel headLabel;
+    private JLabel loginName;
+    private JPanel indexPanel;
+    private JPanel contactPanel;
+    private JPanel clientPanel;
+    private JPanel productPanel;
+    private JPanel requestPanel;
+    private JPanel missionPanel;
+    private JPanel depPanel;
+    private JPanel strongPanel;
+    private JLabel contactLabel;
+    private JLabel clientLabel;
+    private JLabel productLabel;
+    private JLabel requestLabel;
+    private JLabel missionLabel;
+    private JLabel depLabel;
+    private JLabel strongLael;
+    private final CardLayout C;
 
     IndexFrame(){
         init();
@@ -25,6 +44,60 @@ public class IndexFrame extends JFrame{
         UserVo uv=new UserVo();
         headLabel.setBorder(border);
         headLabel.setText("<html><img src='"+uv.getuImg()+"' width='160' height='160'/></html>");
+        loginName.setText(uv.getuName());
+        //创建CardLayout
+        C = new CardLayout();
+        indexPanel.setLayout(C);
+        indexPanel.add("1",contactPanel);
+        indexPanel.add("2",clientPanel);
+        indexPanel.add("3",productPanel);
+        indexPanel.add("4",requestPanel);
+        indexPanel.add("5",missionPanel);
+        indexPanel.add("6",depPanel);
+        indexPanel.add("7",strongPanel);
+        C.show(indexPanel, "1");
+        contactLabel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                C.show(indexPanel, "1");
+            }
+        });
+        clientLabel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                C.show(indexPanel, "2");
+            }
+        });
+        productLabel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                C.show(indexPanel, "3");
+            }
+        });
+        requestLabel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                C.show(indexPanel, "4");
+            }
+        });
+        missionLabel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                C.show(indexPanel, "5");
+            }
+        });
+        depLabel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                C.show(indexPanel, "6");
+            }
+        });
+        strongLael.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                C.show(indexPanel, "7");
+            }
+        });
     }
 
     public void init(){
