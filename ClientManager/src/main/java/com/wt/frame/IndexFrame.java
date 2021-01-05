d4d70a787047809581dd3a6e8ba4f1531e8c74f4
UnKn0Wez <yukikumo@outlook.com>
2021-01-05 08:30:29 +0800

    asd

--- ClientManager/src/main/java/com/wt/frame/IndexFrame.java
+++ ClientManager/src/main/java/com/wt/frame/IndexFrame.java
@@ -21,7 +21,7 @@ import java.util.List;
  * @Author OXH
  * @Date 2020/12/30 9:21
  **/
-public class IndexFrame extends JFrame{
+public class IndexFrame extends JFrame {
     private JPanel mainPanel;
     private JPanel menuPanel;
     private JLabel headLabel;
@@ -51,10 +51,13 @@ public class IndexFrame extends JFrame{
     private JButton button2;
     private JPanel contactBodyPanel;
     private JLabel xField;
+    private JPanel clientContentPanel;
+    private JPanel clientBodyPanel;
+    private JPanel clientSearchPanel;
     private final CardLayout C;
-    private UserVo uv=new UserVo();
+    private UserVo uv = new UserVo();
 
-    IndexFrame(){
+    IndexFrame() {
         init();
         xField.addMouseListener(new MouseAdapter() {
             @Override
@@ -62,9 +65,9 @@ public class IndexFrame extends JFrame{
                 dispose();
             }
         });
-        Border border=new RoundBorder(250,Color.black);
-        Border border1=new RoundBorder(15,Color.decode("#E2E2E2"));
-        Border border2 = new RoundBorder(10,Color.decode("#838383"));
+        Border border = new RoundBorder(250, Color.black);
+        Border border1 = new RoundBorder(15, Color.decode("#E2E2E2"));
+        Border border2 = new RoundBorder(10, Color.decode("#838383"));
         contactSearchButton.setBorder(border2);
         depSearchCombox.setBorder(border2);
         contactSearchText.setBorder(border2);
@@ -72,19 +75,19 @@ public class IndexFrame extends JFrame{
         contactSearchPanel.setBorder(border1);
         contactContentPanel.setBorder(border1);
         headLabel.setBorder(border);
-        headLabel.setText("<html><img src='"+uv.getuImg()+"' width='160' height='160'/></html>");
+        headLabel.setText("<html><img src='" + uv.getuImg() + "' width='160' height='160'/></html>");
         loginName.setText(uv.getuName());
         contactComboxInit();
         //创建CardLayout
         C = new CardLayout();
         indexPanel.setLayout(C);
-        indexPanel.add("1",contactPanel);
-        indexPanel.add("2",clientPanel);
-        indexPanel.add("3",productPanel);
-        indexPanel.add("4",requestPanel);
-        indexPanel.add("5",missionPanel);
-        indexPanel.add("6",depPanel);
-        indexPanel.add("7",strongPanel);
+        indexPanel.add("1", contactPanel);
+        indexPanel.add("2", clientPanel);
+        indexPanel.add("3", productPanel);
+        indexPanel.add("4", requestPanel);
+        indexPanel.add("5", missionPanel);
+        indexPanel.add("6", depPanel);
+        indexPanel.add("7", strongPanel);
         hidePanel();
         contactLabel.addMouseListener(new MouseAdapter() {
             @Override
@@ -131,46 +134,36 @@ public class IndexFrame extends JFrame{
         showContact(ServiceFactory.getUserServiceInstance().selectAll());
     }
 
-    public void showContact(List<ContactVo> contacts){
+    public void showContact(List<ContactVo> contacts) {
         TableModel tableModel;
         tableModel = new DefaultTableModel();
-        JTable table = new JTable(tableModel){ @Override
-        public boolean isCellEditable(int row, int column) { return false; }};
+        JTable table = new JTable(tableModel) {
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return false;
+            }
+        };
         DefaultTableModel model = new DefaultTableModel();
         table.setModel(model);
-        model.setColumnIdentifiers(new String[]{"员工编号","用户名","员工姓名","电话号码","所属部门","负责产品","工资",""});
+        model.setColumnIdentifiers(new String[]{"员工编号", "用户名", "员工姓名", "电话号码", "所属部门", "负责产品", "工资", ""});
         for (ContactVo contact : contacts) {
             Object[] objects = new Object[]{
                     contact.getContactId(),
                     contact.getRealName(), contact.getUserName(),
                     contact.getUserPhone(), contact.getDepName(),
-                    contact.getProductName(),contact.getSalary()
+                    contact.getProductName(), contact.getSalary()
             };
             model.addRow(objects);
         }
-        TableColumn tc = table.getColumnModel().getColumn(7);
-        tc.setMaxWidth(0);
-        tc.setMinWidth(0);
-        JTableHeader header = table.getTableHeader();
-        DefaultTableCellHeaderRenderer hr = new DefaultTableCellHeaderRenderer();
-        hr.setHorizontalAlignment(JLabel.CENTER);
-        header.setDefaultRenderer(hr);
-        header.setPreferredSize(new Dimension(header.getWidth(), 40));
-        header.setFont(new Font("楷体", Font.PLAIN, 18));
-        table.setTableHeader(header);
-        table.setRowHeight(35);
-        table.setBackground(Color.white);
-        DefaultTableCellRenderer r = new DefaultTableCellRenderer();
-        r.setHorizontalAlignment(JLabel.CENTER);
-        table.setDefaultRenderer(Object.class, r);
-        table.setBackground(Color.white);
-        table.setPreferredSize(new Dimension(contactContentPanel.getWidth(),contactContentPanel.getHeight()));
-        JPanel mypane=new JPanel(new BorderLayout());
-        mypane.setPreferredSize(new Dimension(300,table.getRowCount()*table.getRowHeight()));
-        mypane.add(header,BorderLayout.NORTH);
-        mypane.add(table,BorderLayout.CENTER);
-        JScrollPane scrollPane = new JScrollPane(mypane, ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-        scrollPane.setPreferredSize(new Dimension(table.getWidth(),table.getHeight()));
+        JTableHeader header= table.getTableHeader();
+        scrollPanel(table,header);
+        table.setPreferredSize(new Dimension(contactContentPanel.getWidth(), contactContentPanel.getHeight()));
+        JPanel myPanel = new JPanel(new BorderLayout());
+        myPanel.setPreferredSize(new Dimension(300, table.getRowCount() * table.getRowHeight()));
+        myPanel.add(header, BorderLayout.NORTH);
+        myPanel.add(table, BorderLayout.CENTER);
+        JScrollPane scrollPane = new JScrollPane(myPanel, ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        scrollPane.setPreferredSize(new Dimension(table.getWidth(), table.getHeight()));
         scrollPane.setBackground(Color.white);
         contactBodyPanel.add(scrollPane);
         contactBodyPanel.revalidate();
@@ -188,22 +181,41 @@ public class IndexFrame extends JFrame{
         });
     }
 
-    public void contactComboxInit(){
+    public static void scrollPanel(JTable table,JTableHeader header) {
+        TableColumn tc = table.getColumnModel().getColumn(7);
+        tc.setMaxWidth(0);
+        tc.setMinWidth(0);
+        DefaultTableCellHeaderRenderer hr = new DefaultTableCellHeaderRenderer();
+        hr.setHorizontalAlignment(JLabel.CENTER);
+        header.setDefaultRenderer(hr);
+        header.setPreferredSize(new Dimension(header.getWidth(), 40));
+        header.setFont(new Font("楷体", Font.PLAIN, 18));
+        table.setTableHeader(header);
+        table.setRowHeight(35);
+        table.setBackground(Color.white);
+        DefaultTableCellRenderer r = new DefaultTableCellRenderer();
+        r.setHorizontalAlignment(JLabel.CENTER);
+        table.setDefaultRenderer(Object.class, r);
+        table.setBackground(Color.white);
+
+    }
+
+    public void contactComboxInit() {
         depSearchCombox.addItem(Department.builder().depName("请选择部门").depId("1").build());
-        List<Department> departmentList= ServiceFactory.getDepServiceInstance().selectDepAll();
-        for (Department department:departmentList){
+        List<Department> departmentList = ServiceFactory.getDepServiceInstance().selectDepAll();
+        for (Department department : departmentList) {
             depSearchCombox.addItem(department);
         }
     }
 
     //根据身份显示不同的页面
-    public void hidePanel(){
-        String role=uv.getuRole();
-        if("Admin".equals(role)){
+    public void hidePanel() {
+        String role = uv.getuRole();
+        if ("Admin".equals(role)) {
             C.show(indexPanel, "1");
             return;
         }
-        if("Client".equals(role)){
+        if ("Client".equals(role)) {
             C.show(indexPanel, "4");
             clientLabel.setVisible(false);
             contactLabel.setVisible(false);
@@ -211,7 +223,7 @@ public class IndexFrame extends JFrame{
             depLabel.setVisible(false);
             return;
         }
-        if("Contact".equals(role)){
+        if ("Contact".equals(role)) {
             C.show(indexPanel, "2");
             contactLabel.setVisible(false);
             productLabel.setVisible(false);
@@ -220,12 +232,12 @@ public class IndexFrame extends JFrame{
         }
     }
 
-    public void init(){
+    public void init() {
         setContentPane(mainPanel);
         setUndecorated(true);
         setTitle("主页");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(new Dimension(1200,800));
+        setSize(new Dimension(1200, 800));
         setLocationRelativeTo(null);
         setVisible(true);
     }
